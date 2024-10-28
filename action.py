d839af3e25f0b0668d92b33ce6e1f7c041f75fd0
Ansel Robateau <robaone@gmail.com>
2024-10-28 03:03:09 +0000

    feat: support updating existing release and description

--- action.py
+++ action.py
@@ -1,9 +1,33 @@
 from jira import JIRA
 import os
 
-jira = JIRA(server=os.getenv('JIRA_SERVER'),basic_auth=(os.getenv('JIRA_EMAIL'),os.getenv('JIRA_API_TOKEN')))
-is_released = os.getenv('JIRA_RELEASED_STATUS')
-if is_released == 'true':
-    jira.create_version(name=os.getenv('JIRA_VERSION'), project=os.getenv('JIRA_PROJECT'), archived=False, released=True)
+# Connect to JIRA
+jira = JIRA(server=os.getenv('JIRA_SERVER'), basic_auth=(os.getenv('JIRA_EMAIL'), os.getenv('JIRA_API_TOKEN')))
+
+# Fetch environment variables
+is_released = os.getenv('JIRA_RELEASED_STATUS') == 'true'
+release_name = os.getenv('JIRA_VERSION')
+project_key = os.getenv('JIRA_PROJECT')
+release_description = os.getenv('JIRA_RELEASE_DESCRIPTION')
+
+# Check if the version already exists
+existing_versions = jira.project_versions(project_key)
+version = next((v for v in existing_versions if v.name == release_name), None)
+
+if version:
+    # Update the existing version if it already exists
+    version.update(
+        released=is_released,
+        description=release_description
+    )
+    print(f"Updated existing version '{release_name}' in project '{project_key}' with new status and description.")
 else:
-    jira.create_version(name=os.getenv('JIRA_VERSION'), project=os.getenv('JIRA_PROJECT'), archived=False, released=False)
+    # Create the version if it does not exist
+    jira.create_version(
+        name=release_name,
+        project=project_key,
+        archived=False,
+        released=is_released,
+        description=release_description
+    )
+    print(f"Created new version '{release_name}' in project '{project_key}'.")
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ export JIRA_API_TOKEN=$2
 export JIRA_PROJECT=$3
 export JIRA_VERSION=$4
 export JIRA_SERVER=$5
-export JIRA_RELEASED_STATUS=$6
+export JIRA_RELEASE_DESCRIPTION=$6
+export JIRA_RELEASED_STATUS=$7
 python /app/action.py
